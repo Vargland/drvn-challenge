0b944f0ba48a36070c77feaa4988277b7fa107e4
German Roman <groman@makingsense.com>
2025-04-03 01:40:24 -0300

    fix: navigation

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -3,6 +3,7 @@ import { paths } from '@constants/paths'
 import { HomeComponent, ProductComponent } from '@pages'
 
 export const routes: Routes = [
+    { path: paths.HOME, redirectTo: paths.ROOT },
     { path: paths.ROOT, component: HomeComponent },
     { path: paths.PRODUCT, component: ProductComponent },
 ]
--- src/app/containers/navbar/navbar.component.ts
+++ src/app/containers/navbar/navbar.component.ts
@@ -1,16 +1,16 @@
 import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
-import { RouterModule } from '@angular/router';
 
 import { CurrencySelectorComponent } from '@containers';
 
 @Component({
   selector: 'app-navbar',
-  imports: [CommonModule, CurrencySelectorComponent, RouterModule],
+  imports: [CommonModule, CurrencySelectorComponent],
+  providers: [],
   templateUrl: './navbar.component.html',
   styleUrl: './navbar.component.scss'
 })
 
 export default class NavbarComponent {
-    constructor() {}
+    constructor() { }
 }
